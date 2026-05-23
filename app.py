f39f173f9083860dfcc5329551fd7598d4af82fc
langitazmi07-droid <langitazmi07@gmail.com>
2026-05-23 10:05:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-#app.py -baris 13
-st.set_page_config(
-  page_tittle="Finance dashboard",
-  layout="wide"
-)
+#HIRARKI TEKS
+st.tittle("Dashboard")
+st.header("Laporan bulanan")
+st.subheader("Monthly exprnses")
+st.caption("made with love")
