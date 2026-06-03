49560baf5d4019e08939e92378a8e30fa1179fbc
langitazmi07-droid <langitazmi07@gmail.com>
2026-06-04 01:04:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -200,6 +200,7 @@ soal_kuis = [
 st.markdown("""
 .teks-kustom {
     color: #fcd34d !important; /* Warna kuning emas */
+    font-weight: bold;
 }
 @import url('https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:wght@400;600;700&display=swap');
 
@@ -420,7 +421,7 @@ if menu == "🏠 Beranda":
         caption='"Pertemuan dua kepribadian seperti kontak dua zat kimia: Jika ada reaksi, keduanya berubah - CG Jung"',
     )
 
-    st.markdown("<p class='teks-kustom'>💡 Tips: Klik menu Alat Laboratorium di sidebar untuk melihat foto dan penjelasan detail setiap alat, atau langsung coba **Kuis** untuk uji pemahamanmu!</p>", unsafe_allow_html=True)
+    st.markdown("<p class='teks-kustom'>💡 Tips: Klik menu Alat Laboratorium di sidebar untuk melihat foto dan penjelasan detail setiap alat, atau langsung coba Kuis untuk uji pemahamanmu!</p>", unsafe_allow_html=True)
     
     st.markdown("---")
     st.markdown("### ⚡ Preview Alat")
