167af9637b6f1b2aaa8ced70cca2c6bcbca7558d
langitazmi07-droid <langitazmi07@gmail.com>
2026-06-04 00:09:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ hr { border-color: rgba(255,255,255,0.07) !important; }
     background: rgba(255,255,255,0.06) !important;
     border: 1px solid rgba(255,255,255,0.1) !important;
     color: #e2e8f0 !important;
-    border-radius: 10px !important;
+    border-radius: 20px !important;
 }
 </style>
 """, unsafe_allow_html=True)
