37e2574e70bac90e82de0895c31baec565ba5e49
langitazmi07-droid <langitazmi07@gmail.com>
2026-06-06 21:31:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -441,9 +441,22 @@ hr { border-color: rgba(255,255,255,0.07) !important; }
 }
 # Memperbesar ikon toggle sidebar saja
 [data-testid="stSidebarCollapseControl"] button {
-    transform: scale(10); 
+    transform: scale(100); 
     margin-left: 4px;      
 }    
+
+#MEMPERBESAR FONT SIDEBAR
+[data-testid="stSidebarNav"] {
+    font-size: 200px !important;
+    }
+
+[data-testid="stSidebarNav"] li {
+    padding: 200px 0px
+    }
+
+[data-testid="stSidebarNav"] span {
+    font-size: 30px !important;
+    }
 </style>
 """, unsafe_allow_html=True)
 
